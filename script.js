22bc331e5de244e3c11f249391358cdf09aee31e
Sergej Schmidt <sergej-schmidt@online.de>
2025-04-06 00:09:00 +0200

    script

--- script.js
+++ script.js
@@ -1,67 +1,49 @@
-//      https://openweathermap.org/
+// Test Seite
 
-const API_KEY = "5bbad6486ea33c642146f45fcd041e1d";
-const locationInput = document.getElementById("locationInput");
-const getGerichteButton = document.getElementById("getGerichteButton");
-const gerichteContainer = document.getElementById("gerichteContainer");
+const 
+receipts = [
+  {id: 1, title: "Original Burger", price: 11.50, info: "mit Gurken, Tomaten, roten Zwiebeln und Burgersauce"},
+  {id: 2, title: "Spaghetti Napoli", price: 11.00, info: "mit Tomatensauce"},
+  {id: 3, title: "Maccheroni alla Pana", price: 13, info: "mit Champignons, Vorderschinken und Sahnesauce"},
+  {id: 4, title: "Kebap im Fladenbrot", price: 8.5 , info: "Produktinfo"},
+  {id: 5, title: "Kinder Kebap", price: 13, info: "mit einer Beilagen nach Wahl, Salat und Tzatziki"},
+  {id: 6, title: "Türkische Pizza Spezial", price: 10, info: "mit Kebap-Drehspießfleisch und Tzatziki"},
+  {id: 7, title: "Dürüm Kebap", price: 9, info: "mit Kebap-Drehspießfleisch und Tzatziki"},
+  {id: 8, title: "Salat Tasche", price: 7, info: "im Fladenbrot"},
+  {id: 9, title: "Pomm Tasche", price: 7.5, info: "im Fladenbrot mit Pommes frites"}
+];
 
+function findRecieptByTitle(receipts, searchText) {
+  return receipts.filter(obj => 
+    obj.title && obj.title.toLowerCase().includes(searchText.toLowerCase())
+  );
+;}
 
+document.getElementById("gerichte").addEventListener("click", function() {
+  const input = document.getElementById("gerichteInput").value.trim();
+  const ergebnisse = findRecieptByTitle(receipts, input);
 
+  const ausgabe = document.getElementById("gerichteContainer");
+  ausgabe.innerHTML = `<div class="list-group">`;
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-let weatherData;
-
-getGerichteButton.onclick = () => {
-  const cityName = locationInput.value.trim();
-
-  fetch(
-    `https://api.openweathermap.org/data/2.5/weather?q=${cityName}&appid=${API_KEY}&units=metric`
-  )
-    .then((response) => response.json())
-    .then((weather) => {
-      console.log(weather);
-      displayWeather(weather);
+  if (ergebnisse.length > 0) {
+    ergebnisse.forEach(item => {
+      if (item.title = "" ) {ausgabe.innerHTML += "</div>"}
+      else if (condition) {
+        
+      } else {
+        
+      }{
+      ausgabe.innerHTML += `
+                      <button type="button" class="list-group-item list-group-item-action">
+                          <ul class="list-group">
+                        <li class="list-group-item">${item.title}</li>
+                        <li class="list-group-item">${item.price} €</li>
+                        <li class="list-group-item">${item.info} </li>
+                    </ul>
+                    </button>`;
     });
-
-  locationInput.value = "";
-};
-
-function displayWeather({
-  name,
-  main: { temp },
-  weather: [{ description, icon }],
-  wind: { speed: speedOfWind },
-  sys: { sunrise, sunset },
-}) {
-  const sunriseTime = new Date(sunrise * 1000); //  Timestamp * 1000 => in Sekunden
-  const sunsetTime = new Date(sunset * 1000); //  Timestamp * 1000 => in Sekunden
-  console.log(sunriseTime);
-  gerichteContainer.innerHTML = `
-        <div>
-          <h2>${name}</h2>
-          <img src="https://openweathermap.org/img/wn/${icon}.png" alt="icon" />
-        </div>
-        <div>
-          <p>Temperatur: ${temp} °C</p>
-          <p>Description: ${description}</p>
-          <p>speed of Wind: ${speedOfWind} m/s</p>
-          <p>Sunrise: ${sunriseTime.getHours()}:${sunriseTime.getMinutes()}:${sunriseTime.getSeconds()} </p>
-          <p>Sunset: ${sunsetTime.getHours()}:${sunsetTime.getMinutes()}:${sunsetTime.getSeconds()}</p>
-        </div>
-      `;
-    gerichteContainer.classList.add("weatherContainer");
-}
-
+  } else {
+    ausgabe.innerHTML = "<p>Keine Treffer gefunden.</p></div>";
+  }
+});
